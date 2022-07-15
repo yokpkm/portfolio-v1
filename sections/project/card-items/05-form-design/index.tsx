88c788bf32d45b22edf86c13bf9dfd310a2184fa
yokpkm <yok.pkm@gmail.com>
2022-07-15 19:39:35 +0700

    ✨ form design ui

---
+++ sections/project/card-items/05-form-design/index.tsx
@@ -0,0 +1,44 @@
+import { Col, Image, Row } from "antd";
+import { ButtonMore } from "../../../../components/Button";
+import { DividerCard } from "../../../../components/Divider";
+import { StyledTagDesign } from "../../../../components/Tag";
+import { TextContent, TextTitle } from "../../../../components/Text";
+import { FormDesignModal } from "./components/FormDesignModal";
+
+interface Props {
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+}
+
+export const FormDesign: React.FC<Props> = ({ visible, setVisible }) => {
+  return (
+    <>
+      <FormDesignModal visible={visible} setVisible={setVisible} />
+      <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
+        <Image
+          src="/image/card/imgCard05.jpg"
+          preview={false}
+          style={{ borderRadius: 8 }}
+        />
+        <Row style={{ marginTop: 32 }}>
+          <TextTitle>Login / Register Form</TextTitle>
+        </Row>
+        <Row style={{ marginTop: 16 }}>
+          <TextContent>is simply dummy text of the printing.</TextContent>
+        </Row>
+        <Row>
+          <TextContent>of the printing</TextContent>
+        </Row>
+        <DividerCard />
+        <Row gutter={[8, 16]} align="middle">
+          <Col flex="auto">
+            <StyledTagDesign>UI Design</StyledTagDesign>
+          </Col>
+          <Col>
+            <ButtonMore onClick={() => setVisible(true)}>Read more</ButtonMore>
+          </Col>
+        </Row>
+      </Col>
+    </>
+  );
+};
